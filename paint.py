fc35692edfcdf353e257001c0ba7630a8ed2df76
Gilberto Echeverria <gilbertoef@gmail.com>
2021-10-29 08:21:15 -0500

    Change line endings

--- paint.py
+++ paint.py
@@ -66,9 +66,9 @@ def triangle(start, end):
     for count in range(3):
         forward(end.x - start.x)
         left(120)
-    
+
     end_fill
-    
+
 def tap(x, y):
     "Store starting point or draw shape."
     start = state['start']
